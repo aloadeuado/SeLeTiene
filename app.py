8a6a582eafe5c6e3e45c576300a870ef369900fe
aloadeuado <79708828+aloadeuado@users.noreply.github.com>
2023-01-26 23:10:13 -0500

    asdasda

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://Lordviril:Gorposi0717@localhost:27017/")
 db = client['SeLeTiene']
-
-
 
 @app.route("/lologramos", methods=["GET"])
 def lologramos():
