b6f83b4d65e0f5449241a446503d59fb971293f3
aloadeuado <79708828+aloadeuado@users.noreply.github.com>
2023-03-20 15:12:49 -0500

    update conection db

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bson import ObjectId
 
 app = Flask(__name__)
 client = MongoClient("mongodb://Lordviril:Gorposi0717@44.215.48.103:27017/SeLeTiene")
-clientArbelaez = MongoClient("mongodb://miguel:arbelaez@44.215.48.103:27017/ArbelaezApp")
+clientArbelaez = MongoClient("mongodb://essenza:Lordviril0510@44.215.48.103:27017/ArbelaezApp")
 db = client.get_database("SeLeTiene")
 dbArbelaez = clientArbelaez.get_database("ArbelaezApp")
 users = db["Pruebas"]
