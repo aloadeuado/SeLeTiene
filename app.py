42b757310cef15cc3d8f1fa9a4d15573d1bac645
aloadeuado <79708828+aloadeuado@users.noreply.github.com>
2023-05-03 11:56:19 -0500

    fix load

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ from Controllers.ServicesApi import *
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
     
